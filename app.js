e235edd60a9db5d34bd0a0b02133c01bf562fa94
adam-horvath8 <adam.horvath16299@gmail.comm>
2023-05-08 21:27:31 +0200

    Make form content be added to the bookcard

--- app.js
+++ app.js
@@ -1,17 +1,26 @@
-//funkcie
+//constructor
 function book(author, title, numberOfPages, read) {
   this.author = author;
   this.title = title;
-  this.numberOfPages - numberOfPages;
+  this.numberOfPages = numberOfPages;
   this.read = read;
 }
 
 //Selectors
 const newBookBtn = document.getElementById("newBtn");
-const nav = document.querySelector("nav");
-const container = document.querySelector(".container");
+//Form selectors
 const form = document.getElementById("popup");
+const authorInput = document.getElementById("author");
+const titleInput = document.getElementById("title");
+const pagesInput = document.getElementById("numPages");
 const closeButton = document.getElementById("closeBtn");
+const submitButton = document.getElementById("submitForm");
+
+//Card selectors
+const cardDiv = document.getElementById("card");
+const cardHeading = cardDiv.querySelector("h3");
+const cardAuthor = document.getElementById("authorPar");
+const cardPages = document.getElementById("pagesPar");
 
 newBookBtn.addEventListener("click", function () {
   form.style.display = "block";
@@ -21,7 +30,18 @@ closeButton.addEventListener("click", function () {
   form.style.display = "none";
 });
 
+submitButton.addEventListener("click", function (e) {
+  e.preventDefault();
+  const newBook = new book(
+    authorInput.value,
+    titleInput.value,
+    pagesInput.value
+  );
+  cardHeading.innerText = newBook.title;
+  cardAuthor.textContent = newBook.author;
+  cardPages.innerText = newBook.pages;
 
-// const newCard = document.createElement("div");
-// newCard.classList.add("card");
-// container.appendChild(newCard);
+  cardDiv.appendChild(cardHeading);
+  cardDiv.appendChild(cardAuthor);
+  cardDiv.appendChild(cardPages);
+});
