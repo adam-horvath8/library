3f175bbb191119b179fa5a4d930f22fa9bcabde8
adam-horvath8 <adam.horvath16299@gmail.comm>
2023-05-21 20:20:14 +0200

    Style form and button

--- app.js
+++ app.js
@@ -17,7 +17,6 @@ const pagesInput = document.getElementById("numPages");
 const closeButton = document.getElementById("closeBtn");
 const submitButton = document.getElementById("submitForm");
 
-
 newBookBtn.addEventListener("click", function () {
   form.style.display = "flex";
 });
@@ -39,16 +38,22 @@ submitButton.addEventListener("click", function (e) {
 
   const cardHeading = document.createElement("h3");
   cardHeading.innerText = newBook.title;
+  cardHeading.classList.add("cardHeading");
 
   const cardAuthor = document.createElement("p");
   cardAuthor.innerText = newBook.author;
+  cardAuthor.classList.add("cardAuthor");
 
   const cardPages = document.createElement("p");
   cardPages.innerText = newBook.numberOfPages;
 
-  
+  const pagesIcon = document.createElement("i");
+  pagesIcon.classList.add("fa", "fa-solid", "fa-book");
+
   cardDiv.appendChild(cardHeading);
   cardDiv.appendChild(cardAuthor);
   cardDiv.appendChild(cardPages);
   container.appendChild(cardDiv);
+  cardPages.appendChild(pagesIcon);
+  form.reset();
 });
