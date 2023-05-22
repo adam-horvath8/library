3707af029f2e2a87531e133fdd26939879b0996d
adam-horvath8 <adam.horvath16299@gmail.comm>
2023-05-22 17:50:57 +0200

    Add close and read button plus make entering input fields values required

--- app.js
+++ app.js
@@ -14,9 +14,10 @@ const form = document.getElementById("popup");
 const authorInput = document.getElementById("author");
 const titleInput = document.getElementById("title");
 const pagesInput = document.getElementById("numPages");
+const checkInput = document.getElementById("read");
 const closeButton = document.getElementById("closeBtn");
 const submitButton = document.getElementById("submitForm");
-
+// form display
 newBookBtn.addEventListener("click", function () {
   form.style.display = "flex";
 });
@@ -25,8 +26,10 @@ closeButton.addEventListener("click", function () {
   form.style.display = "none";
 });
 
+// submit button
 submitButton.addEventListener("click", function (e) {
   e.preventDefault();
+  if(authorInput.value !== "" && titleInput.value !== "" && pagesInput.value !== ""){
   form.style.display = "none";
   const newBook = new book(
     authorInput.value,
@@ -36,9 +39,21 @@ submitButton.addEventListener("click", function (e) {
   const cardDiv = document.createElement("div");
   cardDiv.classList.add("card");
 
+  const topDiv = document.createElement("div");
+  topDiv.classList.add("topDiv");
+
+  const headingDiv = document.createElement("div");
+  headingDiv.classList.add("headingDiv");
+
   const cardHeading = document.createElement("h3");
   cardHeading.innerText = newBook.title;
   cardHeading.classList.add("cardHeading");
+  //close button
+  const closeBtn = document.createElement("i");
+  closeBtn.classList.add("fas", "fa-times-circle", "cardClose");
+  closeBtn.addEventListener("click", function (e) {
+    cardDiv.style.display = "none";
+  });
 
   const cardAuthor = document.createElement("p");
   cardAuthor.innerText = newBook.author;
@@ -46,14 +61,40 @@ submitButton.addEventListener("click", function (e) {
 
   const cardPages = document.createElement("p");
   cardPages.innerText = newBook.numberOfPages;
+  cardPages.classList.add("cardPages");
 
   const pagesIcon = document.createElement("i");
-  pagesIcon.classList.add("fa", "fa-solid", "fa-book");
+  pagesIcon.classList.add("fa", "fa-solid", "fa-book", "bookIcon");
 
-  cardDiv.appendChild(cardHeading);
+  const readDiv = document.createElement("div");
+  readDiv.classList.add("readDiv");
+  readDiv.addEventListener("click", function () {
+    this.classList.toggle("checked");
+  });
+
+  if (checkInput.checked) {
+    readDiv.className = "checked";
+    readDiv.addEventListener("click", function () {
+      this.classList.toggle("readDiv");
+    });
+  }
+
+  cardDiv.appendChild(topDiv);
+  topDiv.appendChild(closeBtn);
+  cardDiv.appendChild(headingDiv);
+  headingDiv.appendChild(cardHeading);
   cardDiv.appendChild(cardAuthor);
   cardDiv.appendChild(cardPages);
-  container.appendChild(cardDiv);
   cardPages.appendChild(pagesIcon);
+  cardDiv.appendChild(readDiv);
+  container.appendChild(cardDiv);
   form.reset();
+}
+else{
+  alert("Please enter all input fields!")
+}
+});
+
+closeBtn.addEventListener("click", function (e) {
+  cardDiv.style.display = "none";
 });
